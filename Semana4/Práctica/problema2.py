8e3a261b583838da09e6a1729d6a3fcebaf7cd0d
Jonathan <joncortes2212@gmail.com>
2023-02-07 20:23:59 -0600

    update problema2.py"

--- Semana4/Práctica/problema2.py
+++ Semana4/Práctica/problema2.py
@@ -1,3 +1,2 @@
 num = int(input("Ingrese un valor entero: ")) #ingreso de valores
-suma = num*(num+1)/2 #fórmula para suma de valores desde 1 a num
-print(f"La suma de los valores desde 1 hasta {num} es {suma}")
+print(f"La suma de los valores desde 1 hasta {num} es {num*(num+1)/2}")#fórmula para suma de valores desde 1 a num
