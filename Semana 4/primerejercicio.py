8e93f8cfccc15fcacf168f38a400c1194736501d
Jonathan <joncortes2212@gmail.com>
2023-02-07 19:07:18 -0600

    enviar

--- Semana 4/primerejercicio.py
+++ Semana 4/primerejercicio.py
@@ -2,3 +2,11 @@ a = int(input("Ingrese un valor: "))
 if a <= 100:
     print("hola")
 print("Fuera")
+
+
+edad = int(input("Ingrese su edad: "))
+pagar = 1500
+if edad < 3 or edad > 65:
+    pagar = 0
+
+print(f"Debe pagar {pagar} colones")
