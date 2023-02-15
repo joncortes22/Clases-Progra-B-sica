bdcfe841a62e83888c73a23a7f9cabf846dcc909
Jonathan <joncortes2212@gmail.com>
2023-02-14 18:50:14 -0600

    update pruebadefor.py

--- Semana 5/pruebadefor.py
+++ Semana 5/pruebadefor.py
@@ -1,2 +1,15 @@
-for i in range(2,11,2):
+#PRUEBAS DE FOR
+
+print("Primera opción")
+for i in range(10):
+    print(i)
+
+print("\n")
+print("Segunda opción")
+for i in range(5,10):
+    print(i)
+
+print("\n")
+print("Tercera opción")
+for i in range(3,15,5):
     print(i)
---
+++ Semana 5/tablas.py
@@ -0,0 +1,4 @@
+for i in range(1,13):
+    for j in range(1,13):
+        print(i, 'x','=', i*j)
+    print("\n")
