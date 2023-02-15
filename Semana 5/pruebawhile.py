f2ec500fc0e02237da1b0b39fa9b8e4f307d93fe
Jonathan <joncortes2212@gmail.com>
2023-02-14 19:32:57 -0600

    update pruebawhile.py

--- Semana 5/pruebawhile.py
+++ Semana 5/pruebawhile.py
@@ -1,7 +1,14 @@
-resultado = 1
+
+"""resultado = 1
 valor = int(input("Ingrese un valor:"))
 while valor > 0:
     resultado *= valor
     valor -= 1
-print(resultado)
-
+print(resultado)"""
+i = 1
+while i<=12:
+    j = 1
+    while j<=12:
+        print(i,"x",j,"=",i*j)
+        j+=1
+    i+=1
