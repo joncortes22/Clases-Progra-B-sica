455e02cd8de3232aa76897c3645750766bdbc614
Jonathan <joncortes2212@gmail.com>
2023-02-14 20:24:57 -0600

    cambios

--- Semana 5/pares.py
+++ Semana 5/pares.py
@@ -1,3 +1,12 @@
-for i in range(20,41,2):
-    print(f"Numero: {i}")
-    print(f"Numero al cuadrado: {i**2}")
+i = int(input("Ingrese el valor inicial: "))
+f = int(input("Ingrese el valor final: "))
+for i in range(i,f):
+    if i % 2 == 0: print(f"Numero: {i}, y su cuadrado es {i**2}")
+
+
+i = int(input("Ingrese el valor inicial: "))
+f = int(input("Ingrese el valor inicial: "))
+while i<f:
+    i += 1
+    if i % 2 == 0:
+        print(f"{i}, su cuadrado es: {i**2}")
