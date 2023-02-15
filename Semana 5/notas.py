5c71e0a6c97b00e335ad102ceca29d822b5fd3d6
Jonathan <joncortes2212@gmail.com>
2023-02-14 21:02:53 -0600

    fasdf

--- Semana 5/notas.py
+++ Semana 5/notas.py
@@ -1,6 +1,7 @@
 
 estudiantes = 1
 mayor = 0
+menor = 0
 aprobados = 0
 reprobados = 0
 seguir = True
@@ -8,6 +9,8 @@ while seguir == True:
     entrada = int(input(f"Ingrese la nota del estudiante: {estudiantes}: "))
     estudiantes += 1
     if entrada>mayor: mayor = entrada
+    if estudiantes == 0: menor = entrada
+    elif entrada < menor: menor = entrada
     if entrada>=70: aprobados += 1
     else: reprobados += 1
     resp = int(input("Desea seguir ingresando? 1-Si 2-No: "))
@@ -15,6 +18,7 @@ while seguir == True:
 
 print(f"""
 Nota mayor: {mayor}
+Nota mayor: {menor}
 Aprobados: {aprobados}
 Reprobados: {reprobados}""")
 
